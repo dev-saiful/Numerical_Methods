2f363c0c72872f1a6b604d14ffd6f921572ce9c1
Saiful <saifulislamrodro54@gmail.com>
2022-11-21 06:19:24 +0600

    new methods added

---
+++ gauss_seidel.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+#define E 0.001
+#define X(y,z) ((12-2*y-z)/5)
+#define Y(x,z) ((15-x-2*z)/4)
+#define Z(x,y) ((20-x*2y)/5)
+using namespace std;
+int main()
+{
+    float x1=0,y1=0,z1=0,x2,y2,z2;
+    while(1)
+    {
+        x2 = X(y1,z1);
+        y2 = Y(x2,z1);
+        z2 = Y(x2,y2);
+
+        if(fabs(x2-x1)<E && fabs(z2-z1)<E && fabs(y2-y1)<E)
+        {
+            cout<<"Value of x is : "<<x2<<"\n";
+            cout<<"Value of y is : "<<y2<<"\n";
+            cout<<"Value of z is : "<<z2<<"\n";
+            break;
+        }
+        else
+        {
+            x1 = x2;
+            y1 = y2;
+            z1 = z2;
+        }
+    }
+    return 0;
+}
+
